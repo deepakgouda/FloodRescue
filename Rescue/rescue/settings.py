2ebe642b7e4939922034451a2bd5037aff3ab20f
daman98 <anurag.barfa@iitg.ac.in>
2018-10-25 12:37:58 +0530

    make request form updated

--- Rescue/rescue/settings.py
+++ Rescue/rescue/settings.py
@@ -137,4 +137,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER = 'anuragbarfa64@gmail.com'
+EMAIL_HOST_PASSWORD = 'anurag123'
+EMAIL_USE_TLS = True
