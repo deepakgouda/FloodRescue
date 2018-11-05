ffbe234c64b560176a8958a162f6cc87e2f631de
deepakgouda <deepakgouda1729@gmail.com>
2018-11-05 17:07:23 +0000

    Cleaned repo adn added .gitignore

--- Rescue/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-# from .views import ListSongsView
-
-app_name = 'api'
-urlpatterns = [
-    # path('songs/', ListSongsView.as_view(), name="songs-all"),
-    path('shelters/', views.ListShelterView, name="ListShelterView"),
-]
--- Rescue/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'home'
-urlpatterns = [
-    url(r'^$', views.home, name='home1'),
-    url(r'^home/$', views.home, name='home'),
-]
-
